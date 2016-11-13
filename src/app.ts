ee267fd26e210a3a3929976922a6da94214c2e7b
Sam Armstrong <Sam Armstrong>
2016-11-13 14:11:43 -0800

    Adding stub pages

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+
 export class App {
+  router: Router;
   message = 'Hello World!';
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: 'home/index' },
+      { route: 'users', name: 'users', moduleId: 'users/index', nav: true, caseSensitive: true },
+      { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' }
+    ]);
+
+    config.mapUnknownRoutes('not-found');
+  }
 }
